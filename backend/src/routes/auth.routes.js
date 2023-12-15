22de93ca0a590a23077326354773cd2b69e748bf
felipe-ahz <felipe.henriquez2001@alumnos.ubiobio.cl>
2023-12-15 18:28:27 -0300

    pantalla de beneficio en proceso

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -9,9 +9,9 @@ const authController = require("../controllers/auth.controller");
 const router = express.Router();
 
 // Define las rutas para la autenticación
-router.post("/POST/login", authController.login);
-router.post("/POST/logout", authController.logout);
-router.get("/GET/refresh", authController.refresh);
+router.post("/login", authController.login);
+router.post("/logout", authController.logout);
+router.get("/refresh", authController.refresh);
 
 // Exporta el enrutador
 module.exports = router;
